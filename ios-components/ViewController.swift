83089bd63f2d3a68cad3c6963864a188c4a4c560
Eugen Cherniy <eugen.ch.atom@gmail.com>
2017-05-21 19:28:38 +0300

    Alert Component: Implemented alert component with basic example data. Attached to custom button for view.

--- ios-components/ViewController.swift
+++ ios-components/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ButtonAlertListener(_ sender: UIButton) {
+        SetAlertComponent()
+    }
 
+    func SetAlertComponent() {
+        let alertComponent = UIAlertController(title: "Welome message", message: "This is example message test", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertComponent.addAction(alertAction)
+        self.present(alertComponent, animated: true)
+    }
 }
 
