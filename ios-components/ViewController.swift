06a56e5dcba0fedf2362d79ec17fa456f3cd81bb
Eugen Cherniy <eugen.ch.atom@gmail.com>
2017-05-21 18:45:52 +0300

    Initial Commit

---
+++ ios-components/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-components
+//
+//  Created by Eugen Cherniy on 5/21/17.
+//  Copyright © 2017 Eugen Cherniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
